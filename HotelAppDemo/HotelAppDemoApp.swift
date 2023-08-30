338ba133a7704023ad59c0bfe319110217360647
AginS <agins.main@gmail.com>
2023-08-30 17:06:38 +0300

    Initial commit

---
+++ HotelAppDemo/HotelAppDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  HotelAppDemoApp.swift
+//  HotelAppDemo
+//
+//  Created by Vlad Vrublevsky on 30.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct HotelAppDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
